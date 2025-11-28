fc516a1dd28a42215515ad04bab2a363c54f744b
Taranpreet Singh <tsingh@roku.com>
2025-11-28 15:35:14 +0530

    removed the endofdate func

--- internal/util/date.go
+++ internal/util/date.go
@@ -57,7 +57,3 @@ func BeginOfDay(t time.Time) time.Time {
 	year, month, day := t.Date()
 	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 }
-
-func EndOfDay(t time.Time) time.Time {
-	return BeginOfDay(t).AddDate(0, 0, 1)
-}
