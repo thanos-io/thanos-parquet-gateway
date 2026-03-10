5fbfc71923fcc77047431767aa4f32469e074d6f
Craig Ringer <craig.ringer@enterprisedb.com>
2026-03-10 16:44:02 +1300

    tracing: Add support for an OTLP tracer
    Signed-off-by: Craig Ringer <craig.ringer@enterprisedb.com>

--- internal/tracing/config.go
+++ internal/tracing/config.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/thanos-io/thanos/pkg/tracing/jaeger"
+	"github.com/thanos-io/thanos/pkg/tracing/otlp"
 	"go.opentelemetry.io/otel"
 	"gopkg.in/yaml.v2"
 )
@@ -20,12 +21,14 @@ import (
 type TracingProvider string
 
 const (
+	ProviderOTLP   TracingProvider = "OTLP"
 	ProviderJaeger TracingProvider = "JAEGER"
 )
 
 // TracingConfig is the top-level config structure compatible with Thanos.
 type TracingConfig struct {
-	Type TracingProvider `yaml:"type"`
+	Type   TracingProvider    `yaml:"type"`
+	Config map[string]interface{} `yaml:"config"`
 }
 
 // slogToGoKitAdapter adapts slog.Logger to go-kit log.Logger interface.
@@ -63,11 +66,26 @@ func SetupTracingFromConfig(ctx context.Context, logger *slog.Logger, confConten
 		return fmt.Errorf("parsing tracing YAML config: %w", err)
 	}
 
+	// Extract the config section as YAML for the provider
+	configYaml, err := yaml.Marshal(tracingConf.Config)
+	if err != nil {
+		return fmt.Errorf("marshaling config section: %w", err)
+	}
+
 	switch strings.ToUpper(string(tracingConf.Type)) {
+	case string(ProviderOTLP):
+		// Use thanos OTLP package to initialize tracing
+		goKitLogger := slogToGoKitAdapter{logger: logger}
+		tracerProvider, err := otlp.NewTracerProvider(ctx, goKitLogger, configYaml)
+		if err != nil {
+			return fmt.Errorf("creating OTLP tracer provider: %w", err)
+		}
+		otel.SetTracerProvider(tracerProvider)
+		return nil
 	case string(ProviderJaeger):
 		// Use thanos jaeger package to initialize tracing
 		goKitLogger := slogToGoKitAdapter{logger: logger}
-		tracerProvider, err := jaeger.NewTracerProvider(ctx, goKitLogger, confContentYaml)
+		tracerProvider, err := jaeger.NewTracerProvider(ctx, goKitLogger, configYaml)
 		if err != nil {
 			return fmt.Errorf("creating Jaeger tracer provider: %w", err)
 		}
